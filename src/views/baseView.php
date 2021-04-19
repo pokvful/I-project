917eebb90d919548fbba2a4a908309cb4ee0b894
YoloGamer123415 <jorambuitenhuis@gmail.com>
2021-04-19 21:08:50 +0200

    Moved temp directory to settings

--- src/views/baseView.php
+++ src/views/baseView.php
@@ -1,7 +1,9 @@
 <?php
+require_once $_SERVER["DOCUMENT_ROOT"] . '/settings.php';
 require_once $_SERVER["DOCUMENT_ROOT"] . '/vendor/autoload.php';
 
 class BaseView {
+<<<<<<< HEAD
     private Latte\Engine $latteEngine;
 
     public string $requestPath;
@@ -41,5 +43,46 @@ class BaseView {
     public function run() {
         throw new Exception("Not implemented!");
     }
+=======
+	private Latte\Engine $latteEngine;
+
+	public string $requestPath;
+	public string $path;
+	public string $fileName;
+	public array $data;
+
+	public function __construct(string $requestPath, string $path, string $fileName) {
+		$this->latteEngine = new Latte\Engine;
+		$this->latteEngine->setTempDirectory( SETTINGS["latte"]["tempDirectory"] );
+
+		$this->requestPath = $requestPath;
+		$this->path = $path;
+		$this->fileName = $fileName;
+		$this->data = array(
+			"server" => array(
+				"requestPath" => $requestPath,
+				"path" => $path,
+				"fileName" => $fileName,
+			),
+		);
+	}
+
+	protected function redirect(string $path) {
+		header("Location: " . $path);
+	}
+
+	protected function render() {
+		// $this->latteEngine->render($this->path, $this->data);
+		$this->latteEngine->render(
+			$_SERVER["DOCUMENT_ROOT"] . "/src/templates/"
+				. $this->path . $this->fileName . ".latte",
+			$this->data
+		);
+	}
+
+	public function run() {
+		throw new Exception("Not implemented!");
+	}
+>>>>>>> Moved temp directory to settings
 }
 
