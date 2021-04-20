64cd46c4fd01068c3edb092eef0bd0b00c3040fa
pokvful <tim.meuwsen@outlook.com>
2021-04-20 09:30:05 +0200

    Added tabs

--- src/database/databaseHandler.php
+++ src/database/databaseHandler.php
@@ -3,37 +3,37 @@
 require_once $_SERVER["DOCUMENT_ROOT"] . "/settings.php";
 
 class DatabaseHandler {
-    private $connection;
+	private $connection;
 
-    //Constructor
-    public function __construct($connection) {
-        $this->connection = $connection;
-    }
+	//Constructor
+	public function __construct($connection) {
+		$this->connection = $connection;
+	}
 
-    //Lees waarde uit van gebruikers in users tabel
-    private function getUsers() {
-        $sql = "SELECT * FROM users";
-        $stmt = $this->connect()->query($sql);
-        while ($row = $stmt->fetch()) {
-            echo $row['users_firstname'] . '<br>';
-        }
-    }
+	//Lees waarde uit van gebruikers in users tabel
+	private function getUsers() {
+		$sql = "SELECT * FROM users";
+		$stmt = $this->connect()->query($sql);
+		while ($row = $stmt->fetch()) {
+			echo $row['users_firstname'] . '<br>';
+		}
+	}
 
-    //Zet waarde van voor/achternaam binnen users tabel
-    private function setUsers($firstname, $lastname) {
-        $sql = "INSERT INTO users(users_firstname, users_lastname)";
-        $stmt = $this->connect()->query($sql);
-        $stmt->execute([$firstname, $lastname]);
-    }
+	//Zet waarde van voor/achternaam binnen users tabel
+	private function setUsers($firstname, $lastname) {
+		$sql = "INSERT INTO users(users_firstname, users_lastname)";
+		$stmt = $this->connect()->query($sql);
+		$stmt->execute([$firstname, $lastname]);
+	}
 
-    //Maak connectie met database
-    protected function connect() {
-        try {
-            $dsn = "mysql:host=" . SETTINGS["database"]["host"] . ";dbname=" . SETTINGS["database"]["name"] . ";charset=utf8mb4";
-            $this->connection = new PDO($dsn, SETTINGS["database"]["username"], SETTINGS["database"]["password"]);
-            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (PDOException $e) {
-            die("Connection failed: " . $e->getMessage());
-        }
-    }
+	//Maak connectie met database
+	protected function connect() {
+		try {
+			$dsn = "mysql:host=" . SETTINGS["database"]["host"] . ";dbname=" . SETTINGS["database"]["name"] . ";charset=utf8mb4";
+			$this->connection = new PDO($dsn, SETTINGS["database"]["username"], SETTINGS["database"]["password"]);
+			$this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		} catch (PDOException $e) {
+			die("Connection failed: " . $e->getMessage());
+		}
+	}
 }
--- src/database/geen_idee_of_joram_dit_wilt_gebruiken/userView.php
+++ src/database/geen_idee_of_joram_dit_wilt_gebruiken/userView.php
@@ -1,9 +1,8 @@
 <?php
 
 class userView extends Users {
-
-    protected function showUsers($name) {
-        $results = $this->getUser($name);
-        echo "Volledige naam: " . $results['users_firstname'] . $results['users_lastname'];
-    }
+	protected function showUsers($name) {
+		$results = $this->getUser($name);
+		echo "Volledige naam: " . $results['users_firstname'] . $results['users_lastname'];
+	}
 }
