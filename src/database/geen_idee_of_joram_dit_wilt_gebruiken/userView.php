ad2a706bd79987e5700b031c8b5617549027260b
YoloGamer123415 <jorambuitenhuis@gmail.com>
2021-04-22 09:49:08 +0200

    Removed folder

--- src/database/geen_idee_of_joram_dit_wilt_gebruiken/userView.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class userView extends Users {
-
-	protected function showUsers($name) {
-		$results = $this->getUser($name);
-		echo "Volledige naam: " . $results['users_firstname'] . $results['users_lastname'];
-	}
-}
