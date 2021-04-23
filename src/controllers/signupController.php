05ab9badf16f3ab103c120c0beed2b51216fc9fc
YoloGamer123415 <jorambuitenhuis@gmail.com>
2021-04-23 09:07:58 +0200

    Added sign-up step after email send

--- src/controllers/signupController.php
+++ src/controllers/signupController.php
@@ -4,7 +4,11 @@ require_once $_SERVER ["DOCUMENT_ROOT"] . '/src/controllers/baseController.php';
 
 class SignupController extends BaseController {
 	public function run () {
-		$this->data["cameFromMail"] = false;
+		$this->data["cameFromMail"] = true;
+		$this->data["userData"] = array(
+			"email" => "henk@example.com",
+		);
+
 		$this->render();
 	}
 }
