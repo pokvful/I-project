275e726a18e677e086a468a3f345bbd9f42deab5
YoloGamer123415 <jorambuitenhuis@gmail.com>
2021-04-28 09:55:44 +0200

    Removed default constructor

--- src/api/testHandler.php
+++ src/api/testHandler.php
@@ -3,10 +3,6 @@ require_once $_SERVER["DOCUMENT_ROOT"] . '/src/email.php';
 require_once $_SERVER["DOCUMENT_ROOT"] . '/src/api/baseHandler.php';
 
 class TestHandler extends baseHandler {
-	public function __construct(string $requestPath, string $filePath, string $fileName) {
-		parent::__construct($requestPath, $filePath, $fileName);
-	}
-
 	public function run() {
 		$email = new Email("De alleer eerste test mail :)");
 		$email->addAddress("jorambuitenhuis@gmail.com");
