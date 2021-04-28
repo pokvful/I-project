0ec8da96396ebaeddbfbfb802156a05e4dcf1db8
pokvful <tim.meuwsen@outlook.com>
2021-04-28 22:07:57 +0200

    added basic queries to HomeController class for auction items

--- src/api/testHandler.php
+++ src/api/testHandler.php
@@ -4,7 +4,7 @@ require_once $_SERVER["DOCUMENT_ROOT"] . '/src/api/baseHandler.php';
 
 class TestHandler extends BaseHandler {
 	public function run() {
-		$email = new Email("De alleer eerste test mail :)");
+		$email = new Email("De aller eerste test mail :)");
 		$email->addAddress("jorambuitenhuis@gmail.com");
 		$email->setText("Hey, <b>dit</b> is een test");
 		$email->send();
--- src/controllers/homeController.php
+++ src/controllers/homeController.php
@@ -1,8 +1,45 @@
 <?php
 require_once $_SERVER["DOCUMENT_ROOT"] . "/src/controllers/baseController.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/src/database/databaseHandler.php";
 
 class HomeController extends BaseController {
+
+	/**
+	 * Runs homepage queries
+	 */
 	public function run() {
+		/**
+		 * Creates DatabaseHandler object
+		 */
+		$dbh = new DatabaseHandler();
+
+		/**
+		 * Initializes item_number and title variables
+		 */
+		$item_number = 1;
+		$title = "bob";
+
+		/**
+		 * Selects title and item number from vw_Homepage view
+		 */
+//		$this->data["auctionItem"] = $dbh->query("SELECT :title FROM vw_Homepage WHERE item_number = :item_number", array(
+//			":item_number" => $item_number,
+//			":title" => $title,
+//		));
+
+		/**
+		 * Selects all titles from item table
+		 */
+		$this->data["auctionItemTitles"] = $dbh->query("SELECT title FROM vw_Homepage");
+
+		/**
+		 * Selects all descriptions from item table
+		 */
+		$this->data["auctionItemDescriptions"] = $dbh->query("SELECT description FROM vw_Homepage");
+
+		/**
+		 * Outputs data to homepage
+		 */
 		$this->render();
 	}
 }
