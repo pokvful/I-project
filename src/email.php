8009209f57d0e4bdcb26ba8218a87f450fac035b
YoloGamer123415 <jorambuitenhuis@gmail.com>
2021-04-28 09:11:02 +0200

    Removed tags from non-html text

--- src/email.php
+++ src/email.php
@@ -41,7 +41,10 @@ class Email {
 	public function setText(string $body) {
 		// TODO: Maybe add a email template around `$body`
 		$this->mailer->Body = $body;
-		$this->mailer->AltBody = $body; // TODO: String tags?
+		// TODO: Maybe some tags should stay, like links
+		// we could also do some regex parsing to go from `<a href="link">text</a>`
+		// to something like `text (link)`
+		$this->mailer->AltBody = strip_tags($body);
 	}
 
 	public function send() {
