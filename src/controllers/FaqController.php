f6d006acbe8bae94648c9afac59cbf3bedaa89c6
Vincent <VA.Nienhuis@student.han.nl>
2021-04-29 11:49:12 +0200

    Co-authored-by: Joram Buitenhuis <YoloGamer123415@users.noreply.github.com> faq page now syncs with database

--- src/controllers/FaqController.php
+++ src/controllers/FaqController.php
@@ -1,8 +1,24 @@
 <?php
 require_once $_SERVER["DOCUMENT_ROOT"] . "/src/controllers/baseController.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/src/database/databaseHandler.php";
 
 class FaqController extends BaseController {
 	public function run() {
+		$db = new DatabaseHandler();
+
+		if ( !isset( $_GET["questionId"] ) ) {
+			$this->data["items"] = $db->query("SELECT id, questionText FROM Faq");
+		} else {
+			$this->data["questionItem"] = $db->query(
+				"SELECT questionText, answerText FROM Faq WHERE id = :id",
+				array(
+					":id" => $_GET["questionId"]
+				)
+			);
+		}
+
+		$this->data["hasAnswer"] = isset($this->data["questionItem"]) && count( $this->data["questionItem"] ) > 0;
+		
 		$this->render();
-	}		
+	}
 }
