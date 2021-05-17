6dc87aab002877c91bbd90aa5455c9ff7bf8808f
Vincent <VA.Nienhuis@student.han.nl>
2021-05-17 17:11:08 +0200

    changed filenames. made front-end,started back-end

---
+++ src/controllers/sellerSignupController.php
@@ -0,0 +1,12 @@
+<?php
+require_once $_SERVER ["DOCUMENT_ROOT"] . '/src/controllers/baseController.php';
+require_once $_SERVER["DOCUMENT_ROOT"] . "/src/database/databaseHandler.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . '/src/api/baseHandler.php';
+
+
+class SellerSignupController extends BaseController {
+	public function run() {
+
+		$this->render();
+	}
+}
