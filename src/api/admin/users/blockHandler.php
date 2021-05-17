0beade480ae60e515b692b9964a7480ef7597dc5
YoloGamer123415 <jorambuitenhuis@gmail.com>
2021-05-17 13:46:44 +0200

    Added backend backend for admin users (#29)

---
+++ src/api/admin/users/blockHandler.php
@@ -0,0 +1,57 @@
+<?php
+require_once $_SERVER["DOCUMENT_ROOT"] . '/src/database/databaseHandler.php';
+require_once $_SERVER["DOCUMENT_ROOT"] . '/src/api/baseHandler.php';
+
+class BlockHandler extends BaseHandler {
+	public function run() {
+		if ( !isset( $_POST["remove_username"] ) )
+			$this->redirect("/admin/users/");
+
+		$username = $_POST["remove_username"];
+		$db = new DatabaseHandler();
+
+		$users = $db->query(
+			"SELECT blocked FROM [User] WHERE username = :username",
+			array(
+				":username" => $username,
+			),
+		);
+
+		if ( count($users) <= 0 )
+			$this->redirect(
+				"/admin/users/?error=" . urlencode(
+					"De gebruikersnaam \"$username\" staat niet in de database."
+				)
+			);
+
+		$blocked = intval( $users[0]["blocked"] );
+		$newBlocked = $blocked ^ 1;
+
+		try { 
+			$db->query(
+				<<<SQL
+					UPDATE [User]
+						SET blocked = :blocked
+						WHERE username = :username
+				SQL,
+				array(
+					":username" => $username,
+					":blocked" => $newBlocked,
+				)
+			);
+		} catch (Exception $e) {
+			$this->redirect(
+				"/admin/users/?error=" . urlencode(
+					"Er ging iets mis tijdens het (de)blokkeren van een gebruiker: "
+						. $e->getMessage()
+				)
+				);
+		}
+
+		$this->redirect(
+			"/admin/users/?success=" . urlencode(
+				"Succesvol de gebruiker \"$username\" " . ($newBlocked ? "geblokkeerd" : "gedeblokkeerd")
+			)
+		);
+	}
+}
