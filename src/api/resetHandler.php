4d5394b75f89c7388f6bf93bd08b13b4bb03cb5d
JoreyLensink <joreylensink01@gmail.com>
2021-05-17 16:10:48 +0200

    begin resetPassword

--- src/api/resetHandler.php
+++ src/api/resetHandler.php
@@ -3,24 +3,32 @@ require_once $_SERVER["DOCUMENT_ROOT"] . '/src/database/databaseHandler.php';
 require_once $_SERVER["DOCUMENT_ROOT"] . '/src/api/baseHandler.php';
 require_once $_SERVER["DOCUMENT_ROOT"] . '/src/email.php';
 
-class ResetHandler extends BaseHandler {
-	public function run() {
-		$dbh = new DatabaseHandler();
-		/**
-		 * Builds and sends email to users
-		 *
-		 * @param $mail [user] email
-		 * @param $verificationLink ([user] specific) verification code
-		 */
+class ResetHandler extends BaseHandler
+{
+	public function run()
+	{
 
-		function sendVerifyEmail($mail, $verificationLink) {
-			//Builds email that gets sent afterwards
-			$emailBuilder = new Email("Reset password");
-			$emailBuilder->addAddress($mail);
-			$address = (isset($_SERVER['HTTPS']) ? 'https://' : 'http://') . $_SERVER["SERVER_NAME"] . "/signup/";
-			$emailBuilder->setText("Klik op de onderstaande link om een nieuwe wachtwoord in te stellen.<br><a href=\"" . $address . "?hash=" . $verificationLink . "&user=$mail\">Nieuw wachtwoord instellen?</a>");
-			$emailBuilder->send();
-			echo "Done :)";
+		if (isset($_POST["email"])) {
+			$email = $_POST["email"];
+
+			$dbh = new DatabaseHandler();
+			$hashQuery = $dbh->query("SELECT mailbox FROM [User] WHERE mailbox = :mailbox", array(
+				":mailbox" => $email
+			));
+			$this->sendVerifyEmail($email);
 		}
+
+	}
+
+	function sendVerifyEmail($mail)
+	{
+		//Builds email that gets sent afterwards
+		$emailBuilder = new Email("Reset password");
+		$emailBuilder->addAddress($mail);
+		$address = (isset($_SERVER['HTTPS']) ? 'https://' : 'http://') . $_SERVER["SERVER_NAME"] . "/reset/";
+		$emailBuilder->setText("Hallo, dit is een test, klik op onderstaande link om je wachtwoord opnieuw in te stellen <a href=\"" . $address . "?mail=$mail\">Klik hier</a>");
+		$emailBuilder->send();
+		echo "Done :)";
 	}
 }
+
