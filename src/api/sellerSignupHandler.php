615ad1209fdbe2d3b2762d8aa252a286c5931029
Vincent <VA.Nienhuis@student.han.nl>
2021-05-18 16:20:46 +0200

    start seller signup

---
+++ src/api/sellerSignupHandler.php
@@ -0,0 +1,19 @@
+<?php
+require_once $_SERVER["DOCUMENT_ROOT"] . '/src/database/databaseHandler.php';
+require_once $_SERVER["DOCUMENT_ROOT"] . '/src/api/baseHandler.php';
+require_once $_SERVER["DOCUMENT_ROOT"] . '/src/email.php';
+require_once $_SERVER["DOCUMENT_ROOT"] . '/src/api/signupHandler.php';
+
+class SignupHandler extends BaseHandler {
+
+	public function run() {
+		$dbh = new DatabaseHandler();
+
+		$bank = $_POST["bank_name"];
+		$bankAccount = $_POST["bank_account"];
+		$paymentMethod = $_POST["payment_method"];
+		$creditcard = $_POST["creditcard_number"];
+
+		dump($bank,$bankAccount,$paymentMethod,$creditcard);
+	}
+}
