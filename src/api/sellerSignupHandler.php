d67b9f3cba4814da4affc23259a5ce9280e5ae6a
Vincent <VA.Nienhuis@student.han.nl>
2021-05-20 20:21:40 +0200

    added checks added seller check for loading page

--- src/api/sellerSignupHandler.php
+++ src/api/sellerSignupHandler.php
@@ -7,60 +7,71 @@ require_once $_SERVER["DOCUMENT_ROOT"] . '/src/api/signupHandler.php';
 class SellerSignupHandler extends BaseHandler {
 
 	public function run() {
-		$dbh = new DatabaseHandler();
+		$this->data["isSeller"] = $dbh->query("SELECT Seller FROM [User] WHERE [username] = :user",
+		array(
+			":user" => $_SESSION["username"]
+			)
+		);	
+		if($this->data["isSeller"][0]["Seller"] == 1) {
+			$this->redirect("/");
+		} else {
+			$this->render();
+			
+			$dbh = new DatabaseHandler();
 
-		$bank = $_POST["bank_name"];
-		$bankAccount = $_POST["bank_account"];
-		$paymentMethod = $_POST['payment_method'];
-		$creditcard = $_POST["creditcard_number"];
 
-		//Builds URL for signup-errors
-		$addressRoot = (isset($_SERVER['HTTPS']) ? 'https://' : 'http://') . $_SERVER["SERVER_NAME"] . "/sellerSignup/";
-		$redirectAddress = $addressRoot;
+			$bank = $_POST["bank_name"];
+			$bankAccount = $_POST["bank_account"];
+			$paymentMethod = $_POST['payment_method'];
+			$creditcard = $_POST["creditcard_number"];
+			//Builds URL for signup-errors
+			$addressRoot = (isset($_SERVER['HTTPS']) ? 'https://' : 'http://') . $_SERVER["SERVER_NAME"] . "/sellerSignup/";
+			$redirectAddress = $addressRoot;
 
-		//Filters form inputs
-		if (!isset($bank)) {
-			$this->redirect($redirectAddress . "?signup-error=" . urlencode("Banknaam is niet ingevuld.")
+			//Filters form inputs
+			if (!isset($bank) || !$bank) {
+				$this->redirect($redirectAddress . "?signup-error=" . urlencode("Banknaam is niet ingevuld.")
+				);
+			}
+			if (!isset($bankAccount) || !$bankAccount) {
+				$this->redirect($redirectAddress . "?signup-error=" . urlencode("Rekeningnummer is niet ingevuld")
+				);
+			}
+			if (!isset($creditcard) || !$creditcard ) {
+				$this->redirect($redirectAddress . "?signup-error=" . urlencode("Creditcardnummer is niet ingevuld.")
+				);
+			}
+			if ($paymentMethod == 'post' && $creditcard != '') {
+				$this->redirect($redirectAddress . "?signup-error=" . urlencode("U kan geen creditcard ingeven als uw betaalmethode op post staat.")
+				);
+			}
+
+			$dbh->query(
+				<<<SQL
+					INSERT INTO Seller ([user], bank, bank_account, control_option, creditcard)
+									VALUES (:username, :bank, :bank_account, :payment_method, :creditcard_number)
+				SQL,
+				array(
+					":username"	=> $_SESSION["username"],
+					":bank" => $bank,
+					":bank_account" => $bankAccount,
+					":payment_method" => $paymentMethod,
+					":creditcard_number" => $creditcard,
+				)
 			);
-		} else if (!isset($bankAccount)) {
-			$this->redirect($redirectAddress . "?signup-error=" . urlencode("Rekeningnummer is niet ingevuld")
+			$dbh->query(
+				<<<SQL
+				UPDATE 	[User]
+				SET seller = 1
+				WHERE [username] = :username							
+				SQL,
+				array(
+					":username"	=> $_SESSION["username"],
+				)
 			);
-		} else if (!isset($paymentMethod) || ($paymentMethod != 'creditcard' || $paymentMethod != 'post')) {
-			$this->redirect($redirectAddress . "?signup-error=" . urlencode("Betaalmethode is niet ingevuld.")
-			);
-		} else if (!isset($creditcard) || ($paymentMethod == 'creditcard' && $creditcard != '')) {
-			$this->redirect($redirectAddress . "?signup-error=" . urlencode("Creditcardnummer is niet ingevuld.")
-			);
-		} else if ($paymentMethod == 'post' && $creditcard != '') {
-			$this->redirect($redirectAddress . "?signup-error=" . urlencode("U kan geen creditcard ingeven als uw betaalmethode op post staat.")
-			);
+
+			$this->redirect("/profile/");
+
 		}
-
-		$dbh->query(
-			<<<SQL
-				INSERT INTO Seller ([user], bank, bank_account, control_option, creditcard)
-								VALUES (:username, :bank, :bank_account, :payment_method, :creditcard_number)
-			SQL,
-			array(
-				":username"	=> $_SESSION["username"],
-				":bank" => $bank,
-				":bank_account" => $bankAccount,
-				":payment_method" => $paymentMethod,
-				":creditcard_number" => $creditcard,
-			)
-		);
-		$dbh->query(
-			<<<SQL
-			UPDATE 	[User]
-			SET seller = 1
-			WHERE [username] = :username							
-			SQL,
-			array(
-				":username"	=> $_SESSION["username"],
-			)
-		);
-
-		$this->redirect("/profile/");
-
 	}
 }
--- src/controllers/sellerSignupController.php
+++ src/controllers/sellerSignupController.php
@@ -6,7 +6,18 @@ require_once $_SERVER["DOCUMENT_ROOT"] . '/src/api/baseHandler.php';
 
 class SellerSignupController extends BaseController {
 	public function run() {
+		$dbh = new DatabaseHandler();
 		$this->data["signupError"] = $_GET["signup-error"] ?? null;
+		$this->data["isSeller"] = $dbh->query("SELECT Seller FROM [User] WHERE [username] = :user",
+		array(
+			":user" => $_SESSION["username"]
+			)
+		);
+		if($this->data["isSeller"][0]["Seller"] == 1) {
+			$this->redirect("/");
+		} else {
 		$this->render();
+		}
+
 	}
 }
