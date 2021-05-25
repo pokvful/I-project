5171c873caea98e17bab7e983ed7f27277d8b8d0
Vincent <VA.Nienhuis@student.han.nl>
2021-05-25 16:27:23 +0200

    added if for login

--- src/controllers/sellerSignupController.php
+++ src/controllers/sellerSignupController.php
@@ -6,17 +6,21 @@ require_once $_SERVER["DOCUMENT_ROOT"] . '/src/api/baseHandler.php';
 
 class SellerSignupController extends BaseController {
 	public function run() {
-		$dbh = new DatabaseHandler();
-		$this->data["signupError"] = $_GET["signup-error"] ?? null;
-		$this->data["isSeller"] = $dbh->query("SELECT Seller FROM [User] WHERE [username] = :user",
-			array(
-				":user" => $_SESSION["username"]
-			)
-		);
-		if ($this->data["isSeller"][0]["Seller"] == 1) {
+		if (!isset($_SESSION["loggedin"]) || !$_SESSION["loggedin"]) {
 			$this->redirect("/");
 		} else {
-			$this->render();
+			$dbh = new DatabaseHandler();
+			$this->data["signupError"] = $_GET["signup-error"] ?? null;
+			$this->data["isSeller"] = $dbh->query("SELECT Seller FROM [User] WHERE [username] = :user",
+				array(
+					":user" => $_SESSION["username"]
+				)
+			);
+			if ($this->data["isSeller"][0]["Seller"] == 1) {
+				$this->redirect("/");
+			} else {
+				$this->render();
+			}
 		}
 	}
 }
