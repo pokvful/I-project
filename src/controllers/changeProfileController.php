5a5f9f4d25189c46b62e38240429dc7e90b59fa2
Vincent <VA.Nienhuis@student.han.nl>
2021-05-25 16:28:26 +0200

    added data vars for changeProfile

--- src/controllers/changeProfileController.php
+++ src/controllers/changeProfileController.php
@@ -4,6 +4,36 @@ require_once $_SERVER["DOCUMENT_ROOT"] . '/src/database/databaseHandler.php';
 
 class ChangeProfileController extends BaseController {
 	public function run() {
+		$dbh = new DatabaseHandler();
+
+		if (!isset($_SESSION["loggedin"]) || !$_SESSION["loggedin"]) {
+			$this->redirect("/");
+		} else {
+			$this->data["userInformation"] = $dbh->query(
+				"SELECT * FROM [User] WHERE username = :username",
+				array(
+					":username" => $_SESSION["username"]
+				)
+			);
+			$this->data["sellerInformation"] = $dbh->query(
+				"SELECT * FROM Seller WHERE [user] = :user",
+				array(
+					":user" => $_SESSION["username"]
+				)
+			);
+			$this->data["phoneNumber"] = $dbh->query(
+				"SELECT * FROM User_Phone WHERE [user] = :user",
+				array(
+					":user" => $_SESSION["username"]
+				)
+			);
+			$this->data["phoneNumberCount"] = $dbh->query(
+				"SELECT COUNT(*) AS amount FROM User_Phone WHERE [user] = :user",
+				array(
+					":user" => $_SESSION["username"]
+				)
+			);
+		}
 		$this->render();
 	}
 }
