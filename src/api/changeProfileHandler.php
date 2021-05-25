85f29e43f0956eb33edbd29d0451ada8f4b5a999
Vincent <VA.Nienhuis@student.han.nl>
2021-05-25 14:37:58 +0200

    added lay-out changeProfile handler & controller

---
+++ src/api/changeProfileHandler.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once $_SERVER["DOCUMENT_ROOT"] . '/src/database/databaseHandler.php';
+require_once $_SERVER["DOCUMENT_ROOT"] . '/src/api/baseHandler.php';
+
+class ChangeProfileHandler extends BaseHandler {
+
+	public function run() {
+		}
+	}
+}
