fca9bb0fa103aacfed4a865ee08349a73063d688
YoloGamer123415 <jorambuitenhuis@gmail.com>
2021-05-27 19:38:23 +0200

    Not found now sets correct status code

--- src/controllers/notFoundController.php
+++ src/controllers/notFoundController.php
@@ -4,6 +4,9 @@ require_once $_SERVER["DOCUMENT_ROOT"] . "/src/controllers/baseController.php";
 class NotFoundController extends BaseController {
 	public function run() {
 		$this->data["paths"] = explode('/', $this->data["_requestPath"]);
+
+		http_response_code(404);
+
 		$this->render();
 	}
 }
