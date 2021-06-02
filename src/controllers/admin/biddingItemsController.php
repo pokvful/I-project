eb71b49503aa38413871c9148813350b4746f804
pokvful <tim.meuwsen@outlook.com>
2021-06-02 10:51:42 +0200

    renamed faqController added checks to admin pages

--- src/controllers/admin/biddingItemsController.php
+++ src/controllers/admin/biddingItemsController.php
@@ -3,6 +3,20 @@ require_once $_SERVER["DOCUMENT_ROOT"] . "/src/controllers/baseController.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/src/database/databaseHandler.php";
 
 class BiddingItemsController extends BaseController {
+
+	public function checkIfUserIsAdmin() {
+		$dbh = new DatabaseHandler();
+		$checkIfUserIsAdmin = $dbh->query("SELECT * FROM [User] WHERE username = :username AND [admin] = 1", array(
+
+			":username" => $_SESSION["username"]
+
+		));
+
+		if (count($checkIfUserIsAdmin) <= 0) {
+			$this->redirect("/");
+		}
+	}
+
 	private function parseItems(array $items): array {
 		foreach ($items as &$item) {
 			$item["blocked"] = $item["blocked"] == 1;
@@ -11,6 +25,8 @@ class BiddingItemsController extends BaseController {
 	}
 
 	public function run() {
+		$this->checkIfUserIsAdmin();
+
 		if (!$this->data["_admin"] || !$this->data["_loggedin"])
 			$this->redirect("/");
 
